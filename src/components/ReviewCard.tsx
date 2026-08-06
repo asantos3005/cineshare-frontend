4f6152fe9357a09d45496e119d6ad9f64bef1992
Andrew Santos <andrewsan321@gmail.com>
2026-08-06 15:00:20 +1000

    Created seed data for movie reivews and set out basic component fields to be rendered in component.

--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -1,9 +1,13 @@
+import type { ReviewCardData } from '../types/reviewCardData';
 
-export default function ProfileCircle({ reviewObject }: { reviewObject: { movieTitle: string;imageUrl: string; altText: string } }) {
+export default function ProfileCircle({ reviewObject }: { reviewObject: ReviewCardData }) {
   return (
-    <div className="w-10 h-10 rounded-full overflow-hidden border-2 border-neutral-950 dark:border-white">
-        <img src={reviewObject.imageUrl} alt={reviewObject.altText} className="w-full h-full object-cover" />
+    <div className="border-2 border-neutral-950 dark:border-white">
+        <b>{reviewObject.author.username}</b>
+        <b>{reviewObject.reviewedAt}</b>
+        <img src={reviewObject.movieImageUrl} alt={reviewObject.movieImageAlt} className="max-w-xs" />
         <h2>{reviewObject.movieTitle}</h2>
+        <p>{reviewObject.reviewBody}</p>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,21 @@
 import ReviewCard from "../components/ReviewCard";
+import { useEffect, useState } from "react";
+import sampleReviews from "../sample-data/sampleReviews.json";
+import type { ReviewCardData } from "../types/reviewCardData";
 
 function Home(){
+    const [reviews, setReviews] = useState([sampleReviews]);
     return (
         <>
-            <div className="flex flex-col h-screen">
+            <div className="flex flex-col">
                 <h1 className="text-4xl font-bold mb-4">Latest Reviews</h1>
                 <p className="text-lg text-gray-600">See what fellow movie lovers are saying!</p>
             </div>
-            <div className="flex flex-col h-screen">
+            <div className="flex flex-col">
                 {/* Review Cards will go in here */}
+                {reviews.map((review : ReviewCardData) => (
+                    <ReviewCard reviewObject={review} />
+                ))}
             </div>
         </>
     );
